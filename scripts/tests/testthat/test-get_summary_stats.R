5893f30a6b97b7702ba40684a1b2cc105c234091
navyasehgal <navyasehgal11@gmail.com>
2026-03-25 23:01:58 -0700

    Create test-get_summary_stats.R

---
+++ scripts/tests/testthat/test-get_summary_stats.R
@@ -0,0 +1,14 @@
+library(testthat)
+
+test_that("get_summary_stats works correctly", {
+
+  df <- data.frame(category = c("A", "A", "B"))
+
+  result <- get_summary_stats(df, "category")
+
+  expect_equal(nrow(result), 2)
+})
+
+test_that("get_summary_stats errors on bad input", {
+  expect_error(get_summary_stats("not_df", "col"))
+})
