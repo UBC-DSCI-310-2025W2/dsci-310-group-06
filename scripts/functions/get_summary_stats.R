8a7acb235a6f13e12bc0157f39af9038dbbea977
navyasehgal <navyasehgal11@gmail.com>
2026-03-25 22:59:24 -0700

    Update get_summary_stats.R

--- scripts/functions/get_summary_stats.R
+++ scripts/functions/get_summary_stats.R
@@ -1,3 +1,13 @@
+#' Get summary statistics for a categorical variable
+#'
+#' @param df A data frame
+#' @param column Column name (string)
+#' @param coerce.char Logical, whether to convert column to character
+#' @param new.key Name for the output column
+#'
+#' @return A data frame with counts for each category
+#' @export
+
 get_summary_stats <- function(df, column, coerce.char = FALSE, new.key = column) {
   
   if (!is.data.frame(df)) {
